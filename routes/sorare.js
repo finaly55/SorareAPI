949d3e6b62c6752fc41649eb8940ba5eb2ac72e0
Julien LEBATARD <jlebatard@afdtech.com>
2023-07-16 22:16:45 +0200

    ajout de commentaires

--- routes/sorare.js
+++ routes/sorare.js
@@ -5,14 +5,15 @@ moment.locale('fr')
 const fs = require('fs');
 
 const router = express.Router();
-/**
- * Get flight by Id
- */
+
+// Définir la route GET '/getAllPlayers'
 router.get('/getAllPlayers', async (req, res) => {
+  // Initialiser les objets de réponse
   let responseInitial = {};
   let responseNext = {};
 
   try {
+    // Définir la requête GraphQL à envoyer à l'API
     const requestBody = {
       query: `
       query Football {
@@ -44,18 +45,23 @@ router.get('/getAllPlayers', async (req, res) => {
         }
     }`
     };
+
+    // Lire le contenu du fichier JSON précédemment enregistré
     const fileRead = fs.readFileSync(__dirname + '/../players.json', 'utf8');
     responseInitial = JSON.parse(fileRead);
 
+    // Effectuer une requête POST vers l'API GraphQL pour récupérer les joueurs
     responseInitial = await postRequest('/federation/graphql', requestBody);
     let count = 0;
 
+    // Boucler tant qu'il y a des pages suivantes à récupérer
     while (
       responseInitial.data.football && responseInitial.data.football.allCards.pageInfo.hasNextPage
     ) {
       count = count + 1;
       console.log(count);
 
+      // Définir la requête GraphQL pour la page suivante
       const requestBody = {
         query: `
         query Football {
@@ -91,28 +97,33 @@ router.get('/getAllPlayers', async (req, res) => {
       }`
       };
 
+      // Effectuer une requête POST pour récupérer la page suivante de joueurs
       responseApi = await postRequest('/federation/graphql', requestBody);
       if (responseApi.data && responseApi.data.football) {
         responseNext = responseApi
 
+        // Ajouter les joueurs de la page suivante à la liste de joueurs
         responseInitial.data.football.allCards.nodes.push(
           ...responseNext.data.football.allCards.nodes
         );
+        // Mettre à jour les informations de pagination
         responseInitial.data.football.allCards.pageInfo =
           responseNext.data.football.allCards.pageInfo;
       }
     }
 
-    // Enregistrer la réponse dans un fichier
+    // Enregistrer la réponse dans un fichier JSON
     fs.writeFileSync(__dirname + '/../players.json', JSON.stringify(responseInitial));
 
+    // Renvoyer la réponse au client
     res.json(responseInitial);
   } catch (error) {
     console.log(error)
+    // En cas d'erreur, enregistrer la réponse initiale dans le fichier JSON
     fs.writeFileSync(__dirname + '/../players.json', JSON.stringify(responseInitial));
+    // Renvoyer la réponse initiale au client
     res.json(responseInitial);
   }
-
 });
 
 router.get("/getAllSorarePlayers", async (req, res) => {
